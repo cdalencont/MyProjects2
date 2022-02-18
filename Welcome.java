de0e1c94d88e900b30885e42a75e7c9ddc4b7e3f
cdalencont <ca00333@ess.ais.ac.nz>
2022-02-18 16:51:53 +1300

    3try

--- Welcome.java
+++ Welcome.java
@@ -1,8 +1,8 @@
 public class Welcome
-
 {
 	public static void main(String[]args)
 	{
-		System.out.println("Wlecome to SOFT806 course !")
+		System.out.println("to SOFT806 course !")
+		System.out.println("to SOFT806 course !")
 	}
 }
